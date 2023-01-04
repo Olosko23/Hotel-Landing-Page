a1949e9d6706a022e5d0c617c69b83ec60d70f17
Olosko23 <59167901+Olosko23@users.noreply.github.com>
2023-01-04 15:36:33 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import one from './assets/1.jpg';
 import three from './assets/3.jpg';
 import ten from './assets/10.jpg';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
+import {Drinks,Testimonials,Snacks, Courses} from './Data';
+import {Link} from 'react-scroll';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
+  
   return (
     <div className={darkMode ? "dark" : ""}>
-      <main className="h-full px-8 gap-4 flex flex-col bg-[#FEFBF3] dark:bg-[#5199b1]">
+      <main className="h-full px-8 gap-4 flex flex-col bg-[#FEFBF3] dark:bg-[#47869b]">
         <section className="pt-5">
         <nav className="flex flex-row gap-5 justify-between">
           <p className="font-semibold text-2xl py-2 px-1">Hotel</p>
           <BsFillMoonStarsFill size={30} onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl pt-2" />
-          <button className="py-3 px-6 bg-gradient-to-r from-teal-500 to-blue-500 rounded-md font-semibold">Order Here</button>
+          <Link to="order" smooth duration={500} className=" cursor-pointer py-3 px-6 bg-gradient-to-r from-teal-500 to-blue-500 rounded-md font-semibold">Order Here</Link>
         </nav>
         <div className="h-screen grid place-items-center">
           <div>
@@ -38,38 +41,34 @@ function App() {
         <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
           <div className="text-center shadow-lg p-10 rounded-xl my-10">
             <h4 className="font-semibold text-xl justify-center pb-4">Drinks and Beverage</h4>
-            <p>Soda @60</p>
-            <p>Blended Fruit Juice @50</p>
-            <p>Tea @30</p>
-            <p>Coffee @40</p>
+            {
+              Drinks.map(({id,title,price}) => (
+                <p className="capitalize gap-2"key={id}>{title} @{price}</p>
+              ))
+            }
             <img className="rounded-xl pb-0"src={three} alt="new"/>
           </div>
           <div className="text-center shadow-lg p-10 rounded-xl my-10">
             <h4 className="font-semibold text-xl justify-center pb-4">Starters and Snacks</h4>
-            <p>Samosa @30</p>
-            <p>Chapati @20</p>
-            <p>Kebab @40</p>
-            <p>Fries @70 - Medium</p>
-            <p>Fries @100 - Large</p>
-            <p>Bhajia @70</p>
-            <p>Sausages @40</p>
-            <p>Smokies @30</p>
+            {
+              Snacks.map(({id,title,price}) => (
+                <p key={id} className="capitalize gap-2">{title} @{price}</p>
+              ))
+            }
             <img className="rounded-xl pb-0"src={one} alt="new"/>
           </div>
           <div className="text-center shadow-lg p-10 rounded-xl my-10">
             <h4 className="font-semibold text-xl justify-center pb-4">Main Courses</h4>
-            <p>Beans@40</p>
-            <p>Mbuzi Fry @200</p>
-            <p>Beef Fry @200</p>
-            <p>Beef Stew @200</p>
-            <p>Tilapia Dry Fry @200</p>
-            <p>Tilapia Stew @200</p>
-            <p>Matumbo Fry @200</p>
+            {
+              Courses.map(({id,title,price}) => (
+                <p key={id} className="capitalize gap-2">{title} @{price}</p>
+              ))
+            }
             <img className="rounded-xl pb-0"src={ten} alt="new"/>
           </div>
         </div>
       </section>
-      <section className="w-full pb-5">
+      <section name="order"className="w-full pb-5">
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full shadow-lg rounded-xl">
             <div className="pb-8 flex flex-row justify-center">
                 <p className="py-6 text-3xl sm:text-4xl font-medium">Submit the form to place an order</p>
@@ -77,14 +76,14 @@ function App() {
             <div className="flex justify-center items-center">
                 <form action="https://getform.io/f/94770f33-2cf2-411e-8c9f-bd1a03122049" method= "POST" className="flex flex-col w-full md:w-1/2">
                     <input type="text" name="name" placeholder="Enter Your Name" className="p-2 bg-transparent border-2 rounded-md
-                    text-white focus:outline-none"/>
+                    text-gray-700 focus:outline-none"/>
                     <input type="phone" name="phone" placeholder="Enter Your Phone" className="p-2 bg-transparent border-2 rounded-md
-                    text-white focus:outline-none my-4"/>
+                    text-gray-700 focus:outline-none my-4"/>
                     <input type="text" name="order" placeholder="Your Order" className="p-2 bg-transparent border-2 rounded-md
-                    text-white focus:outline-none my-4"/>
+                    text-gray-700 focus:outline-none my-4"/>
                     <input type="text" name="quantity" placeholder="Your Order" className="p-2 bg-transparent border-2 rounded-md
-                    text-white focus:outline-none my-4"/>
-                    <textarea name="message" rows="10"placeholder="Enter Your Message" className="p-2 bg-transparent border-2 rounded-md focus:outlined-none"/>
+                    text-gray-700 focus:outline-none my-4"/>
+                    <textarea name="message" rows="5"placeholder="Any additional information" className="p-2 bg-transparent border-2 rounded-md focus:outlined-none"/>
                     <button className="text-white bg-gradient-to-b from-cyan-500 to-teal-500 px-6 py-3 my-8 mx-auto
                     flex items-center rounded-md hover:scale-110 duration-300">Proceed to Checkout</button>
                 </form>
@@ -95,26 +94,15 @@ function App() {
       <h3 className="text-3xl font-semibold flex flex-row justify-around pt-3">Testimonials</h3>
         <div className="grid place-content-center place-items-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 py-10">
-          <div className="flex-col gap-4 text-center shadow-lg p-10 rounded-xl my-10">
-            <h3 className="text-xl font-medium">George Collins</h3>
-            <p className="font-normal text-gray-500">Quality service and affordable rates,Quality service and affordable rates,Quality service and affordable rates</p>
-            <p className="text-gray-500 hover:text-gray-700 duration-300 cursor-pointer">Rating: <span>4.3/5.0</span></p>
-          </div>
-          <div className="flex-col gap-4 text-center shadow-lg p-10 rounded-xl my-10">
-            <h3 className="text-xl font-medium">George Collins</h3>
-            <p className="font-normal text-gray-500">Quality service and affordable rates,Quality service and affordable rates,Quality service and affordable rates</p>
-            <p className="text-gray-500 hover:text-gray-700 duration-300 cursor-pointer">Rating: <span>4.3/5.0</span></p>
-          </div>
-          <div className="flex-col gap-4 text-center shadow-lg p-10 rounded-xl my-10">
-            <h3 className="text-xl font-medium">George Collins</h3>
-            <p className="font-normal text-gray-500">Quality service and affordable rates,Quality service and affordable rates,Quality service and affordable rates</p>
-            <p className="text-gray-500 hover:text-gray-700 duration-300 cursor-pointer">Rating: <span>4.3/5.0</span></p>
-          </div>
-          <div className="flex-col gap-4 text-center shadow-lg p-10 rounded-xl my-10">
-            <h3 className="text-xl font-medium">George Collins</h3>
-            <p className="font-normal text-gray-500">Quality service and affordable rates,Quality service and affordable rates,Quality service and affordable rates</p>
-            <p className="text-gray-500 hover:text-gray-700 duration-300 cursor-pointer">Rating: <span>4.3/5.0</span></p>
-          </div>
+            {
+              Testimonials.map(({id,title,opinion,rating}) => (
+                <div key={id} className="flex-col gap-4 text-center shadow-lg p-10 rounded-xl my-10">
+                <h3 className="text-xl font-medium capitalize">{title}</h3>
+                <p className="font-normal text-gray-700 py-3 capitalize">{opinion}</p>
+                <p className="text-gray-700 hover:text-gray-700 duration-300 cursor-pointer">Rating: <span>{rating}/5.0</span></p>
+              </div>
+              ))
+            }
           </div>
         </div>
       </section>
---
+++ src/Data.jsx
@@ -0,0 +1,162 @@
+
+    export const Drinks = [
+        {
+          id:1,
+          title: "tea",
+          price: "30"
+        },
+        {
+          id:2,
+          title: "soda 500ml",
+          price: "60"
+        },
+        {
+          id:3,
+          title: "soda 300ml",
+          price: "40"
+        },
+        {
+          id:4,
+          title: "coffee",
+          price: "40"
+        },
+        {
+          id:5,
+          title: "blended Fruit Juice",
+          price: "50"
+        },
+        {
+          id:6,
+          title: "Minute Maid",
+          price: "80"
+        },
+        {
+          id:7,
+          title: "Energy Drink",
+          price: "60"
+        },
+        {
+          id:8,
+          title: "Dasani 500ml",
+          price: "40"
+        },
+      ]
+    
+      export const Snacks = [
+        {
+          id: 1,
+          title:"samosa",
+          price:"30"
+        },
+        {
+          id: 2,
+          title:"chapati",
+          price:"20"
+        },
+        {
+          id: 3,
+          title:"kebab",
+          price:"40"
+        },
+        {
+          id: 4,
+          title:"fries - Medium",
+          price:"70"
+        },
+        {
+          id: 5,
+          title:"fries - Large",
+          price:"100"
+        },
+        {
+          id: 6,
+          title:"bhajia",
+          price:"70"
+        },
+        {
+          id: 7,
+          title:"sausages",
+          price:"40"
+        },
+        {
+          id: 8,
+          title:"smokies",
+          price:"30"
+        }
+      ]
+    
+      export const Testimonials = [
+        {
+          id: 1,
+          title: "George Collins",
+          opinion: "Best services in town, I would 100% recommend",
+          rating: "5.0",
+        },
+        {
+          id: 2,
+          title: "Collins Oloo",
+          opinion: "I would recommend to my friends",
+          rating: "4.1",
+        },
+        {
+          id: 3,
+          title: "Olosko George",
+          opinion: "perfect services and nice ambience",
+          rating: "4.8",
+        },
+        {
+          id: 4,
+          title: "George Oloo",
+          opinion: "Quality food but pricy",
+          rating: "3.2",
+        },
+        {
+          id: 5,
+          title: "Olosko Oloo",
+          opinion: "Quality food and friendly service",
+          rating: "4.3",
+        },
+      ]
+      
+      export const Courses = [
+        {
+          id: 1,
+          title: "beans",
+          price: "40"
+        },
+        {
+          id: 2,
+          title: "Mbuzi Fry",
+          price: "200"
+        },
+        {
+          id: 3,
+          title: "Beef Fry",
+          price: "200"
+        },
+        {
+          id: 4,
+          title: "Beef Stew",
+          price: "200"
+        },
+        {
+          id: 5,
+          title: "Tilapia Dry Fry",
+          price: "200"
+        },
+        {
+          id: 6,
+          title: "Tilapia Stew",
+          price: "200"
+        },
+        {
+          id: 7,
+          title: "Matumbo Fry",
+          price: "200"
+        },
+        {
+          id: 8,
+          title: "chicken",
+          price: "200"
+        },
+      ]
